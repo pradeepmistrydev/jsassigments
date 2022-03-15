8275be4ef4a43647edcda231129e16cd0fe2efe1
pradeepmistrydev <96470628+pradeepmistrydev@users.noreply.github.com>
2022-03-15 14:47:09 +0530

    Update login.js

--- EmailLogin/login.js
+++ EmailLogin/login.js
@@ -4,52 +4,6 @@ let SubmitNode = document.getElementById("Submit");
 let RESETNode = document.getElementById("RESET");
 let border1 = "1px solid red";
 let border2 = "1px solid green";
-function validate1() {
-    let fname = fnameNode.value;
-    Tdnode1.textContent = "";
-    let regExp = new RegExp("^[A-Za-z]");
-    if (fname == "") {
-        Tdnode1.textContent = "Please add your name";
-        fnameNode.style.border = border1;
-    }
-    else if (regExp.test(fname) == false) {
-        Tdnode1.textContent = "name should have only alphabets";
-    }
-    else {
-        fnameNode.style.border = "2px solid green";
-    }
-}
-function validate2() {
-    let lname = lnameNode.value;
-    Tdnode2.textContent = "";
-    let regExp = new RegExp("[A-Za-z]");
-    if (lname == "") {
-        Tdnode2.textContent = "Please add your name";
-        lnameNode.style.border = border1;
-    }
-    else if (regExp.test(lname) == false) {
-        Tdnode2.textContent = "name should have only alphabets";
-    }
-    else {
-        lnameNode.style.border = "2px solid green";
-    }
-}
-function validate3() {
-    let Username = UsernameNode.value;
-    Tdnode3.textContent = "";
-    let regExp = new RegExp("^[A-Za-z]");
-    if (lname == "") {
-        Tdnode3.textContent = "Please add your name";
-        UsernameNode.style.border = border1;
-    }
-    else if (regExp.test(Username) == false) {
-        Tdnode3.textContent = "name should have only alphabets";
-    }
-    else {
-        UsernameNode.style.border = "2px solid green";
-    }
-
-}
 function validate4() {
     let Password = PasswordNode.value;
     Tdnode4.textContent = "";
